9d7aa9ecb6931117d0f6d8849447c7b6099cc56d
SolDapper <soldapper.com@gmail.com>
2025-02-18 14:35:49 -0500

    modified:   README.md 	modified:   mcbuild.js 	modified:   package.json

--- README.md
+++ README.md
@@ -16,7 +16,8 @@ import mcbuild from './mcbuild.js';
 
 // optimize transaction
 const _tx_ = {};
-_tx_.rpc = rpc;                       // string : required
+_tx_.rpc = rpc;     
+_tx_.blink = false;                   // bool   : default false
 _tx_.account = payer;                 // string : required
 _tx_.instructions = [instruction];    // array  : required
 _tx_.signers = false;                 // array  : default false
@@ -29,29 +30,14 @@ _tx_.fees = true;                     // bool   : default true
 _tx_.priority = "Medium";             // string : VeryHigh,High,Medium,Low,Min
 const tx = await mcbuild.tx(_tx_);    // mcbuild
 
-// stop for errors
-if(typeof tx.logs != "undefined"){
-  console.log("error", tx);
-  return;
+if(typeof tx.status!="undefined"){console.log(tx);}
+else{
+    const signed = await provider.signTransaction(tx);
+    const signature = await mcbuild.send(rpc,signed);
+    console.log("signature", signature);
+    console.log("awaiting status...");
+    const status = await mcbuild.status(rpc,signature);
+    if(status!="finalized"){console.log("status", status);}
+    else{console.log(status);}
 }
-
-try{
-
-  // sign, serialize, and send transaction
-  const signed = await provider.signTransaction(tx);
-  const signature = await connection.sendRawTransaction(
-    signed.serialize(),
-    {skipPreflight:true,maxRetries:0}
-  );     
-  
-  // track the status
-  // default: 10 checks max for tx confirmation, check in 4 second intervals
-  const status = await mcbuild.status(rpc,signature,10,4); 
-  console.log("status", status);
-
-}
-catch(err){
-  console.log("err", err);
-}
-
 ```
--- mcbuild.js
+++ mcbuild.js
@@ -8,31 +8,40 @@ class mcbuild {
     constructor() {
       this.name = "mcbuild";
     }
+    async send(rpc,tx){
+        try{
+            const connection = new Connection(rpc,"confirmed");
+            const signature = await connection.sendRawTransaction(tx.serialize(),{skipPreflight:true,maxRetries:0});
+            return signature;
+        }
+        catch(err){
+            const _error_ = {}
+            _error_.status="error";
+            _error_.message=err;
+            return _error_;
+        }
+    }
     async status(cluster,sig,max=10,int=4){
-        return await new Promise(resolve => {
+        return await new Promise(resolve=>{
             let start = 1;
             let connection = null;
             connection = new Connection(cluster, "confirmed");
             let intervalID = setInterval(async()=>{
             let tx_status = null;
             tx_status = await connection.getSignatureStatuses([sig], {searchTransactionHistory: true,});
-            console.log(start+": "+sig);
-            if (tx_status != null && typeof tx_status.value != "undefined"){ 
-                console.log(tx_status.value);
-            }
-            else{
-                console.log("failed to get status...");
-            }
             if (tx_status == null || 
             typeof tx_status.value == "undefined" || 
             tx_status.value == null || 
             tx_status.value[0] == null || 
             typeof tx_status.value[0] == "undefined" || 
-            typeof tx_status.value[0].confirmationStatus == "undefined"){} 
+            typeof tx_status.value[0].confirmationStatus == "undefined"){
+                console.log("trying again...");
+            } 
             else if(tx_status.value[0].confirmationStatus == "processed"){
                 start = 1;
             }
             else if(tx_status.value[0].confirmationStatus == "confirmed"){
+                console.log("confirming...");
                 start = 1;
             }
             else if (tx_status.value[0].confirmationStatus == "finalized"){
@@ -51,63 +60,35 @@ class mcbuild {
             },(int * 1000));
         });  
     }
-    async ComputeLimit(cluster,opti_payer,opti_ix,opti_tolerance,blockhash,opti_tables=false){
-        let connection = new Connection(cluster, 'confirmed');
-        let opti_sim_limit = ComputeBudgetProgram.setComputeUnitLimit({units:1400000});
-        let opti_fee_limit = ComputeBudgetProgram.setComputeUnitPrice({microLamports:10000});
+    async ComputeLimit(cluster,opti_payer,opti_ix,opti_tolerance,blockhash,opti_table=false){
+        const connection = new Connection(cluster, 'confirmed');
+        const opti_sim_limit = ComputeBudgetProgram.setComputeUnitLimit({units:1400000});
+        const opti_fee_limit = ComputeBudgetProgram.setComputeUnitPrice({microLamports:10000});
         let re_ix = [];
         for (let o in opti_ix) {re_ix.push(opti_ix[o]);}
         opti_ix = re_ix;
         opti_ix.unshift(opti_sim_limit);
         opti_ix.unshift(opti_fee_limit);
         let opti_msg = null;
-        if(opti_tables == false){
-            opti_msg = new TransactionMessage({
-            payerKey: opti_payer.publicKey,
-            recentBlockhash: blockhash,
-            instructions: opti_ix,
-            }).compileToV0Message([]);
-        }
-        else{
-            console.log("opti_payer", opti_payer.toString());
-            opti_msg = new TransactionMessage({
-            payerKey: opti_payer.publicKey,
-            recentBlockhash: blockhash,
-            instructions: opti_ix,
-            }).compileToV0Message(opti_tables);
-        }
-        let opti_tx = new VersionedTransaction(opti_msg);    
-        let opti_cu_res = await connection.simulateTransaction(opti_tx,{replaceRecentBlockhash:true,sigVerify:false,});
-        console.log("Simulation Results: ", opti_cu_res.value);
+        opti_msg = new TransactionMessage({payerKey:opti_payer.publicKey,recentBlockhash:blockhash,instructions:opti_ix,}).compileToV0Message(opti_table);
+        const opti_tx = new VersionedTransaction(opti_msg);    
+        const opti_cu_res = await connection.simulateTransaction(opti_tx,{replaceRecentBlockhash:true,sigVerify:false,});
         if(opti_cu_res.value.err != null){
-            return {"message":"error during simulation","logs":opti_cu_res.value.logs}
+            return {"status":"error","message":"simulation error","details":opti_cu_res.value.err,"logs":opti_cu_res.value.logs};
         }
-        let opti_consumed = opti_cu_res.value.unitsConsumed;
-        let opti_cu_limit = Math.ceil(opti_consumed * opti_tolerance);
+        const opti_consumed = opti_cu_res.value.unitsConsumed;
+        const opti_cu_limit = Math.ceil(opti_consumed * opti_tolerance);
+        console.log("setting cu limit", opti_cu_limit);
         return opti_cu_limit;
     }
-    async FeeEstimate(cluster,payer,priorityLevel,instructions,blockhash,tables=false){
-        let connection = new Connection(cluster,'confirmed',);
+    async FeeEstimate(cluster,payer,priority_level,instructions,blockhash,table=false){
+        const connection = new Connection(cluster,'confirmed',);
         let re_ix = [];
         for (let o in instructions) {re_ix.push(instructions[o]);}
         instructions = re_ix;
-        let _msg = null;
-        if(tables==false){
-            _msg = new TransactionMessage({
-            payerKey: payer.publicKey,
-            recentBlockhash: blockhash,
-            instructions: instructions,
-            }).compileToV0Message([]);
-        }
-        else{
-            _msg = new TransactionMessage({
-            payerKey: payer.publicKey,
-            recentBlockhash: blockhash,
-            instructions: instructions,
-            }).compileToV0Message(tables);
-        }
-        let tx = new VersionedTransaction(_msg);
-        let response = await fetch(cluster, {
+        const _msg = new TransactionMessage({payerKey:payer.publicKey,recentBlockhash:blockhash,instructions:instructions,}).compileToV0Message(table);
+        const tx = new VersionedTransaction(_msg);
+        const response = await fetch(cluster, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
@@ -117,65 +98,57 @@ class mcbuild {
             params: [
                 {
                 transaction: bs58.encode(tx.serialize()), // Pass the serialized transaction in Base58
-                options: { priorityLevel: priorityLevel },
+                options: { priorityLevel: priority_level },
                 },
             ],
             }),
         });
         let data = await response.json();
-        console.log("estimate reponse:", data);
         data = parseInt(data.result.priorityFeeEstimate);
+        if(data == 1){data = 100000;}
         if(data < 10000){data = 10000;}
+        console.log("fee estimate", data);
         return data;
     }
     async tx(_data_){
-    let _obj_={};let _rpc_;let _account_;let _instructions_;let _signers_;let _priority_;let _tolerance_;let _serialize_;let _encode_;let _table_;let _compute_;let _fees_;
-    if(typeof _data_.rpc=="undefined"){_obj_.message="missing rpc";return _obj_;}else{_rpc_=_data_.rpc;}
-    if(typeof _data_.account=="undefined"){_obj_.message="missing account";return _obj_;}else{_account_=_data_.account;}
-    if(typeof _data_.instructions=="undefined"){_obj_.message="missing instructions";return _obj_;}else{_instructions_=_data_.instructions;}
-    if(typeof _data_.signers=="undefined"){_signers_=false;}else{_signers_=_data_.signers;}
-    if(typeof _data_.priority=="undefined"){_priority_="Medium";}else{_priority_=_data_.priority;}
-    if(typeof _data_.tolerance=="undefined"){_tolerance_="1.1";}else{_tolerance_=_data_.tolerance;}
-    if(typeof _data_.serialize=="undefined"){_serialize_=false;}else{_serialize_=_data_.serialize;}
-    if(typeof _data_.encode=="undefined"){_encode_=false;}else{_encode_=_data_.encode;}
-    if(typeof _data_.table=="undefined"){_table_=false;}else{_table_=_data_.table;}
-    if(typeof _data_.compute=="undefined"){_compute_=true;}else{_compute_=_data_.compute;}
-    if(typeof _data_.fees=="undefined"){_fees_=true;}else{_fees_=_data_.fees;}
-    let _wallet_= new PublicKey(_account_);
-    let connection = new Connection(_rpc_,"confirmed");
-    let _blockhash_ = (await connection.getLatestBlockhash('confirmed')).blockhash;
-    if(_priority_=="Extreme"){_priority_="VeryHigh";}
-    let _payer_={publicKey:_wallet_}
-    if(_compute_ != false){
-        let _cu_ = null;
-        _cu_= await this.ComputeLimit(_rpc_,_payer_,_instructions_,_tolerance_,_blockhash_,_table_);
-        if(typeof _cu_.logs != "undefined"){
-            _cu_.message="there was an error when simulating the transaction";
-            return _cu_;
-        }
-        else if(_cu_==null){
-            _obj_.message="there was an error when optimizing compute limit";
-            return _obj_;
-        }
-        _instructions_.unshift(ComputeBudgetProgram.setComputeUnitLimit({units:_cu_}));
+        let _obj_={};let _rpc_;let _account_;let _instructions_;let _signers_;let _priority_;let _tolerance_;let _serialize_;let _encode_;let _table_;let _compute_;let _fees_;
+        if(typeof _data_.rpc=="undefined"){_obj_.message="missing rpc";return _obj_;}else{_rpc_=_data_.rpc;}
+        if(typeof _data_.account=="undefined"){_obj_.message="missing account";return _obj_;}else{_account_=_data_.account;}
+        if(typeof _data_.instructions=="undefined"){_obj_.message="missing instructions";return _obj_;}else{_instructions_=_data_.instructions;}
+        if(typeof _data_.signers=="undefined" || _data_.signers==false){_signers_=false;}else{_signers_=_data_.signers;}
+        if(typeof _data_.priority=="undefined"){_priority_="Low";}else{_priority_=_data_.priority;}
+        if(typeof _data_.tolerance=="undefined"){_tolerance_="1.1";}else{_tolerance_=_data_.tolerance;}
+        if(typeof _data_.serialize=="undefined"){_serialize_=false;}else{_serialize_=_data_.serialize;}
+        if(typeof _data_.encode=="undefined"){_encode_=false;}else{_encode_=_data_.encode;}
+        if(typeof _data_.compute=="undefined"){_compute_=true;}else{_compute_=_data_.compute;}
+        if(typeof _data_.fees=="undefined"){_fees_=true;}else{_fees_=_data_.fees;}
+        if(typeof _data_.table=="undefined" || _data_.table==false){_table_=[];}else{_table_=[_data_.table];}
+        const _wallet_= new PublicKey(_account_);
+        const connection = new Connection(_rpc_,"confirmed");
+        const _blockhash_ = (await connection.getLatestBlockhash('confirmed')).blockhash;
+        if(_priority_=="Extreme"){_priority_="VeryHigh";}
+        let _payer_={publicKey:_wallet_}
+        if(_compute_ != false){
+            let _cu_ = null;
+            _cu_= await this.ComputeLimit(_rpc_,_payer_,_instructions_,_tolerance_,_blockhash_,_table_);
+            if(typeof _cu_.logs != "undefined"){
+                _obj_.status="error";
+                _cu_.message="there was an error when simulating the transaction";
+                return _cu_;
+            }
+            else if(_cu_==null){
+                _obj_.status="error";
+                _obj_.message="there was an error when optimizing compute limit";
+                return _obj_;
+            }
+            _instructions_.unshift(ComputeBudgetProgram.setComputeUnitLimit({units:_cu_}));
         }
         if(_fees_ != false){
-            let get_priority = await this.FeeEstimate(_rpc_,_payer_,_priority_,_instructions_,_blockhash_,_table_);
+            const get_priority = await this.FeeEstimate(_rpc_,_payer_,_priority_,_instructions_,_blockhash_,_table_);
             _instructions_.unshift(ComputeBudgetProgram.setComputeUnitPrice({microLamports:get_priority}));
         }
-        let _message_=null;
-        console.log("_wallet_", _wallet_.toString());
-        console.log("_blockhash_", _blockhash_.toString());
-        console.log("_table_", _table_);
-        if(_table_!=false){
-            console.log("using table");
-            _message_ = new TransactionMessage({payerKey:_wallet_,recentBlockhash:_blockhash_,instructions:_instructions_,}).compileToV0Message(_table_);
-        }
-        else{
-            console.log("not using table");
-            _message_ = new TransactionMessage({payerKey:_wallet_,recentBlockhash:_blockhash_,instructions:_instructions_,}).compileToV0Message([]);
-        }
-        let _tx_= new VersionedTransaction(_message_);
+        let _message_ = new TransactionMessage({payerKey:_wallet_,recentBlockhash:_blockhash_,instructions:_instructions_,}).compileToV0Message(_table_);
+        let _tx_ = new VersionedTransaction(_message_);
         if(_signers_!=false){
             _tx_.sign(_signers_);
         }
@@ -189,6 +162,7 @@ class mcbuild {
             _obj_ = _tx_;
         }
         else{
+            _obj_.status="ok";
             _obj_.message="success";
             _obj_.transaction=_tx_;
         }
